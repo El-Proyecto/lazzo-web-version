cf373d03f29a1162c053e498e59a40289119bbbe
Guilherme Monteiro <guilherme.dias.monteiro@tecnico.ulisboa.pt>
2025-11-08 13:30:34 +0000

    same format for all app bars and lateral padding fixed

--- lib/features/create_event/presentation/widgets/create_event_app_bar.dart
+++ lib/features/create_event/presentation/widgets/create_event_app_bar.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import '../../../../shared/constants/spacing.dart';
 import '../../../../shared/constants/text_styles.dart';
 import '../../../../shared/themes/colors.dart';
 
@@ -29,58 +28,55 @@ class CreateEventAppBar extends StatelessWidget implements PreferredSizeWidget {
       backgroundColor: Colors.transparent,
       elevation: 0,
       automaticallyImplyLeading: false,
-      title: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: Insets.screenH),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            // Botão Back
-            GestureDetector(
-              onTap: onBackPressed ?? () => Navigator.of(context).pop(),
-              child: Container(
-                width: 32,
-                height: 32,
-                alignment: Alignment.center,
-                child: const Icon(
-                  Icons.arrow_back_ios,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          // Botão Back
+          GestureDetector(
+            onTap: onBackPressed ?? () => Navigator.of(context).pop(),
+            child: Container(
+              width: 32,
+              height: 32,
+              alignment: Alignment.center,
+              child: const Icon(
+                Icons.arrow_back_ios,
+                color: BrandColors.text1,
+                size: 20,
+              ),
+            ),
+          ),
+
+          // Título
+          Expanded(
+            child: Center(
+              child: Text(
+                title, // Use the title parameter instead of hard-coded
+                style: AppText.dropdownTitle.copyWith(
                   color: BrandColors.text1,
-                  size: 20,
+                  fontSize: 22,
+                  fontWeight: FontWeight.w500,
                 ),
+                textAlign: TextAlign.center,
               ),
             ),
+          ),
 
-            // Título
-            Expanded(
-              child: Center(
-                child: Text(
-                  title, // Use the title parameter instead of hard-coded
-                  style: AppText.dropdownTitle.copyWith(
+          // Botão Histórico ou ação customizada
+          trailingAction ??
+              GestureDetector(
+                onTap: onHistoryPressed,
+                child: Container(
+                  width: 32,
+                  height: 32,
+                  alignment: Alignment.center,
+                  child: const Icon(
+                    Icons.history,
                     color: BrandColors.text1,
-                    fontSize: 22,
-                    fontWeight: FontWeight.w500,
+                    size: 20,
                   ),
-                  textAlign: TextAlign.center,
                 ),
               ),
-            ),
-
-            // Botão Histórico ou ação customizada
-            trailingAction ??
-                GestureDetector(
-                  onTap: onHistoryPressed,
-                  child: Container(
-                    width: 32,
-                    height: 32,
-                    alignment: Alignment.center,
-                    child: const Icon(
-                      Icons.history,
-                      color: BrandColors.text1,
-                      size: 20,
-                    ),
-                  ),
-                ),
-          ],
-        ),
+        ],
       ),
     );
   }
