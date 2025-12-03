490c3115c2fc7db067f6fb27f5d2a9312b21f034
Guilherme Monteiro <guilherme.dias.monteiro@tecnico.ulisboa.pt>
2025-12-03 17:26:19 +0000

    edit profile switched to bottom sheets

--- lib/shared/components/components.dart
+++ lib/shared/components/components.dart
@@ -31,6 +31,8 @@ export 'common/create_event_segmented_control.dart';
 export 'common/top_banner.dart';
 export 'common/invite_bottom_sheet.dart';
 export 'common/simple_selection_sheet.dart';
+export 'common/edit_field_bottom_sheet.dart';
+export 'common/birthday_picker_bottom_sheet.dart';
 
 // Section Components
 export 'sections/event_header.dart';
